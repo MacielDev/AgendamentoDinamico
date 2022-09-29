3e0b806586ecfb5cc76cb74a16815958c5cb07f2
MacielDev <andreraizdetudo@gmail.com>
2022-09-28 21:45:09 -0300

    Finalizando o projeto

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,22 +1,14 @@
+import { eventosAsync } from './seletores/index';
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
+import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
 
 export const listaDeEventosState = atom<IEvento[]>({
   key: "listaDeEventosState",
-  default: [
-    {
-      descricao: "Estudar React",
-      inicio: new Date("2022-01-15T09:00"),
-      fim: new Date("2022-01-15T13:00"),
-      completo: false,
-      id: 1642342747,
-    },
-    {
-      descricao: "Estudar Recoil",
-      inicio: new Date("2022-01-16T09:00"),
-      fim: new Date("2022-01-16T11:00"),
-      completo: false,
-      id: 1642342959,
-    },
-  ],
+  default: eventosAsync // utilizando o seletor que realiza a busca dos eventos de nossa "API do Json server"
 });
+
+export const filtroDeEventos = atom<IFiltroDeEventos>({
+  key: 'filtroDeEventos',
+  default: {}
+})
